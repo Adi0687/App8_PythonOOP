68e9f95dda4cd892bce13f8095c38f1c4b051291
Aadil Feroze <ferozeaadil@gmail.com>
2021-10-25 23:04:25 +0300

    definition class addedd

--- definition.py
+++ definition.py
@@ -1,7 +1,17 @@
+import pandas
+
+
 class Definition:
 
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv("data.csv")
+        result = tuple(df.loc[df['word'] == self.term]['definition'])
+        return result
+
+
+if __name__ == "--main__":
+    acid_rain = Definition(term="acid rain")
+    print(acid_rain.get())
